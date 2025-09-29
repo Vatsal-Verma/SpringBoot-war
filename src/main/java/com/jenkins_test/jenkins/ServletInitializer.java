d9dd5c45302b5eda6a9a75444445340420eab590
Psychedelics <146416700+Vatsal-Verma@users.noreply.github.com>
2025-09-29 22:55:43 +0530

    Update ServletInitializer.java

--- src/main/java/com/jenkins_test/jenkins/ServletInitializer.java
+++ src/main/java/com/jenkins_test/jenkins/ServletInitializer.java
@@ -1,5 +1,4 @@
 package com.jenkins_test.jenkins;
-
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
