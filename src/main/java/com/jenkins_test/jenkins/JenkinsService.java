a7c6e26e30acb9619f716062db63f540408805d8
Psychedelics <146416700+Vatsal-Verma@users.noreply.github.com>
2025-11-06 23:28:28 +0530

    Correct method and class name in JenkinsService
    Fix method declaration and class name typo.

--- src/main/java/com/jenkins_test/jenkins/JenkinsService.java
+++ src/main/java/com/jenkins_test/jenkins/JenkinsService.java
@@ -1,6 +1,8 @@
+// this methos is only for learning purpose that's why it does not contain any packages and imports. 
 
 class JnekinsService {
-    
+
+    @Transaction
     class String getName() {
         return getName();    
     }
